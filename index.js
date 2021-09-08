0564fb6c2bd97b98306dc439dc7aa26df63fb8fc
Sajal Gupta <guptasajal411@gmail.com>
2021-09-08 17:56:53 +0530

    feat: :rocket: Bootstrapify Register and Login pages, Add public folder

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.set("view engine", "ejs");
 app.use(express.json());
 
 app.use("/", require(path.join(__dirname, "./routes/routes.js")));
+app.use(express.static("public"));
 
 module.exports = app;
